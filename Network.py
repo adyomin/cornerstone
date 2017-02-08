936148396a1a8b74271f421900daaf37b3d8ad2f
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-08 19:48:50 +0300

    Add Layer.backward() method implementation + lots of clean ups

--- Network.py
+++ Network.py
@@ -10,7 +10,7 @@ class Network():
     """
 
     def __init__(self, features, targets, h_size, activation,
-                 activation_prime):
+                 activation_prime, eta):
 
         # To-Do-List:
         # - add proper choice of activation function & activation_prime
@@ -20,9 +20,11 @@ class Network():
         # targets have to be of shape (n_records, n_targets)
         self._activation = activation
         self._activation_prime = activation_prime
-        self.shape = (features.shape[1], tuple(h_size), targets.shape[1])
         self._features = features
         self._targets = targets
+        self._eta = eta
+
+        self.shape = (features.shape[1], tuple(h_size), targets.shape[1])
 
     def train(self):
         pass
@@ -33,17 +35,28 @@ class Network():
 
 class Layer(Network):
 
-    def __init__(self, network, previous, next, width):
-        self._network = network
+    def __init__(self, previous, next, width):
         self._previous = previous
+        self._input = previous._output
         self._next = next
         self._width = width
         self._weights = np.random.normal(0,
                                          scale=previous._width**(-0.5),
                                          size= (previous._width, self._width)
                                          )
+        self._arg = None
+        self._output = None
 
-    def forward(self, batch):
+        if self._next != None:
+            # (batch_size, self._width)
+            self._d_cost_d_output = next._d_cost_d_input
+        else:
+            # This looks bad :-(
+            self._d_cost_d_output = None
+
+
+
+    def forward(self, batch=None):
 
         """
         Updates current layer output based on its wights and previous
@@ -55,8 +68,7 @@ class Layer(Network):
         batch : numpy.array
             Has to be of size (batch_size, self.shape[0]).  At the
             moment it is only needed for input layer (self._previous ==
-            None).  Has to be numpy.array or similar.  No conversion
-            implemented atm (saves time @ RT).
+            None).  Has to be numpy.array or similar.
 
         Return
         ----------
@@ -66,13 +78,63 @@ class Layer(Network):
 
         # TODO Layer.forward(..., batch) - fix argument list
         # TODO Layer.forward(...) - add exceptions ?
+        # TODO Layer.forward(...) - add matrix sizes
 
         if self._previous != None:
-            self._input = np.dot(self._previous._output, self._weights)
-            self._output = self._network._activation(self._input)
+            self._arg = np.dot(self._input, self._weights)
+            self._output = self._activation(self._arg)
         else:
             self._output = batch
 
 
-    def backward(self):
-        pass
+    def backward(self, eta=0.001, d_cost_d_output=None):
+
+        """
+        Updates layer's weights based on d cost/d output and passes d cost/d
+        input down the line.
+
+        Parameters
+        ----------
+
+        eta : float
+            Learning rate.
+
+        d_cost_d_output: numpy.array
+            Has to be of size (batch_size, self.width).  At the
+            moment it is only needed for the output layer (self._next ==
+            None).  Has to be numpy.array or similar.
+
+        Return
+        ----------
+
+        None.
+        """
+
+        if self._next == None:
+            # (batch_size, self._width)
+            self._d_cost_d_output = d_cost_d_output
+
+        # (batch_size, self._width)
+        self._d_output_d_arg = self._activation_prime(self._arg)
+
+        # (batch_size, self._width)
+        # d cost/d arg = d cost/d output * d output/d arg
+        self._d_cost_d_arg = np.multiply(self._d_cost_d_output,
+                                         self._d_output_d_arg)
+
+        # (previous._width, self._width)
+        self._d_arg_d_input = self._weights
+
+        # has to be (batch_size, previous._width)
+        # d cost/d input = d cost/d arg * d cost/d input
+        self._d_cost_d_input = np.matmul(self._d_cost_d_arg,
+                                         self._d_arg_d_input.T)
+        # (batch_size, previous._width)
+        self._d_arg_d_weights = self._input
+
+        # has to be (previous._width, self._width)
+        # d cost/d wights = d cost/d arg * d arg/d weights
+        self._d_cost_d_weights = np.matmul(self._d_arg_d_weights.T,
+                                           self._d_cost_d_arg)
+
+        self._weights += -eta*self._d_cost_d_weights
