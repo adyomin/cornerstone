a8829607f09276c4d23830d4cc5028617c3a6732
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-09 18:15:29 +0300

    Use PEP8 formatting for demo.py

--- demo.py
+++ demo.py
@@ -4,17 +4,18 @@ import numpy as np
 
 
 features = pd.read_csv('~/Yandex.Disk.localized/Projects'
-                       '/deep_learining_program/week1/project_1/features.csv')
+                       '/deep_learning_program/week1/project_1/features.csv')
 targets = pd.read_csv('~/Yandex.Disk.localized/Projects'
-                      '/deep_learining_program/week1/project_1/targets.csv')
+                      '/deep_learning_program/week1/project_1/targets.csv')
 
-x = np.array(features.ix[:,1:])
+x = np.array(features.ix[:, 1:])
 print(x.shape)
-y = np.array(targets.ix[:,2:])
+y = np.array(targets.ix[:, 2:])
 print(y.shape)
 
 size = 512
-n = 1024
+n = 100
+np.random.seed(42)
 
-test = net.Network(features=x, targets=y, h_size= (56, 28), eta=0.001)
+test = net.Network(features=x, targets=y, h_size=(112, 28), eta=0.001)
 test.train(batch_size=size, n_epochs=n)
