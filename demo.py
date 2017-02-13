b6cb6d1c18b16ad92c0e1c4599615da72374f668
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-13 14:49:52 +0300

    Update demo for new path to the data

--- demo.py
+++ demo.py
@@ -3,17 +3,15 @@ import pandas as pd
 import numpy as np
 
 
-features = pd.read_csv('~/Yandex.Disk.localized/Projects'
-                       '/deep_learning_program/week1/project_1/features.csv')
-targets = pd.read_csv('~/Yandex.Disk.localized/Projects'
-                      '/deep_learning_program/week1/project_1/targets.csv')
+features = pd.read_csv('.data/features.csv')
+targets = pd.read_csv('.data/targets.csv')
 
 x = np.array(features.ix[:, 1:])
 y = np.array(targets.ix[:, 2:])
 
 size = 512
-n = 128
+n = 512
 np.random.seed(42)
 
-test = net.Network(features=x, targets=y, h_size=(112, 28), eta=0.001)
+test = net.Network(features=x, targets=y, h_size=(56, 28, 14, 7), eta=0.005)
 test.train(batch_size=size, n_epochs=n)
