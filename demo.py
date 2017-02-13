5ecaccc724a5b1a3e91477c4cf774aa76953831c
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-13 15:14:30 +0300

    Merge AFs and AFPs definitions with Network class + make Network.__init__() Layer free.
    Network.__init__ now only takes network size tuple (width of all layers, starting from the input width == n_features), activation functions (AF; separate functions for all hidden layers and the output layer; this defines  activation function primes (AFPs) as well with the same layer logic), and cost function.  I am ditching the Layer class gradually, merging everything into the Network class.
    
    Current commit contains major Network.__init__ update to reflect the above as well as Network.evaluate() initial implementation from the last week.

--- demo.py
+++ demo.py
@@ -15,3 +15,5 @@ np.random.seed(42)
 
 test = net.Network(features=x, targets=y, h_size=(56, 28, 14, 7), eta=0.005)
 test.train(batch_size=size, n_epochs=n)
+
+#pushing from PyCharm using token thingie
