99fdb4ed13c0a3bba0fc9242977ed25489f7478d
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-14 13:08:36 +0300

    Clean up demo calls to reduce warnings and errors.

--- demo.py
+++ demo.py
@@ -12,8 +12,3 @@ y = np.array(targets.ix[:, 2:])
 size = 512
 n = 512
 np.random.seed(42)
-
-test = net.Network(features=x, targets=y, h_size=(56, 28, 14, 7), eta=0.005)
-test.train(batch_size=size, n_epochs=n)
-
-#pushing from PyCharm using token thingie 2
