3f4cda2dfd2310f4f9eea845b9407c593e63ad07
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-14 15:46:49 +0300

    Update folder structure.

--- demo/bike_rental/bike_rental_demo.py
+++ demo/bike_rental/bike_rental_demo.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 
 
-features = pd.read_csv('./data/bike_rental/features.csv')
-targets = pd.read_csv('./data/bike_rental/targets.csv')
+features = pd.read_csv('./data/features.csv')
+targets = pd.read_csv('./data/targets.csv')
 
 x = np.array(features.ix[:, 1:])
 y = np.array(targets.ix[:, 2:])
