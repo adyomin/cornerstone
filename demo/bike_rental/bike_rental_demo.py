3349fe60d1526e572fb42c5b7ff33bbfc02daf42
Andrei Dyomin <andrei.dyomin@gmail.com>
2017-02-14 17:10:56 +0300

    Update bike demo for new data split.

--- demo/bike_rental/bike_rental_demo.py
+++ demo/bike_rental/bike_rental_demo.py
@@ -3,15 +3,19 @@ import pandas as pd
 import numpy as np
 
 
-features = pd.read_csv('./data/features.csv')
-targets = pd.read_csv('./data/targets.csv')
+train_features = pd.read_csv('./data/train_features.csv')
+train_targets = pd.read_csv('./data/train_targets.csv')
+val_features = pd.read_csv('./data/val_features.csv')
+val_targets = pd.read_csv('./data/val_targets.csv')
 
-x = np.array(features.ix[:, 1:])
-y = np.array(targets.ix[:, 2:])
+x_train = np.array(train_features.ix[:, 1:])
+y_train = np.array(train_targets.ix[:, 2:])
+x_val = np.array(val_features.ix[:, 1:])
+y_val = np.array(val_targets.ix[:, 2:])
 
 size = 128
 n = 512
 np.random.seed(42)
 
 test = net.Network(size=(56, 28, 14, 7, 2))
-test.train(x, y, batch_size=size, eta=0.025, n_epochs=n)
+test.train(x_train, y_train, batch_size=size, eta=0.025, n_epochs=n)
